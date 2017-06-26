02338f94a4dc9ed9d799c3c1d35e558bcf176640
eagle1111 <wang499817054@gmail.com>
2017-06-26 20:32:02 +0800

    订单折线资料分析

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,11 +6,24 @@ class Admin::OrdersController < ApplicationController
 
   def index
     @orders = Order.order("id DESC")
+
+    dates = (@orders.order("id ASC").first.created_at.to_date..Date.today).to_a
+
+     @data1 = {
+       labels: dates,
+       datasets: [{
+         data: dates.map{ |d|
+             @orders.where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count},
+         borderColor: "42A5F5"
+         }]
+     }
+
   end
 
   def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
+
   end
 
   def ship
